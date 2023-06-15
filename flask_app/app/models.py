3ff68337edab0a400ebe4783934cbb1a7644e8fa
Anna <anna.rogala@gmail.com>
2023-06-15 21:16:19 +0200

    ISSUE-5 Add Bootstrap and style existing layouts

--- flask_app/app/models.py
+++ flask_app/app/models.py
@@ -4,8 +4,6 @@ from datetime import datetime
 from app import db
 
 
-
-
 class Plant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
